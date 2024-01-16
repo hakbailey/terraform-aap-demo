802e8157ecdac6d8da72b0cb560e2f83e7645016
Helen Bailey <hebailey@redhat.com>
2024-01-16 18:39:12 -0500

    Add second instance

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -13,10 +13,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "created-by-terraform" {
+resource "aws_instance" "created-by-terraform-1" {
   ami           = "ami-0005e0cfe09cc9050"
   instance_type = "t2.micro"
   tags = {
-    Name = "created-by-terraform"
+    Name = "created-by-terraform-1"
+  }
+}
+
+resource "aws_instance" "created-by-terraform-2" {
+  ami           = "ami-0005e0cfe09cc9050"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "created-by-terraform-2"
   }
 }
